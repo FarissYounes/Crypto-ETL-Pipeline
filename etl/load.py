c57f9eaa8ef64ca3586f653c0085db9df7b7ff42
Frobinus <younes.fariss.yf@gmail.com>
2025-11-13 18:20:09 +0100

    Add : load_data function for standardized data loading

---
+++ etl/load.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from utils.transformation import DataType, load_data
+
+def load_data(file_name : str, data_type : DataType) -> pd.DataFrame :
+    return load_data(data_type, file_name)
--- utils/transformation.py
+++ utils/transformation.py
@@ -7,12 +7,16 @@ class DataType(Enum):
     RAW = "data/raw"
     PROCESSED = "data/processed"
 
-def get_storage_path(data_type: DataType, filename: str) -> Path:
+def get_storage_path(data_type: DataType, file_name: str) -> Path:
     base_path = Path(data_type.value)
-    path = base_path / filename
+    path = base_path / file_name
     return path
 
 def save_file(file_name: str, data: list, data_type: DataType) :
     path = get_storage_path(data_type, file_name)
     df = pd.DataFrame(data)
     df.to_csv(path)
+
+def load_data(file_name: str, data_type: DataType) -> pd.DataFrame :
+    df = get_storage_path(data_type, file_name)
+    return df
